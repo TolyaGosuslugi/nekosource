7c25241b310483ee27244704a69f1a9be8fec11c
TolyaGosuslugi <nikita2010belov@gmail.com>
2024-12-30 22:08:47 +0500

    yappii clone working

--- src/buttons.h
+++ src/buttons.h
@@ -4,25 +4,45 @@
 #include <QSize>
 #include <QLayout>
 #include <QWidget>
+#include <string>
 #include <QStyleFactory>
 #include <QLineEdit>
 #include <QAction>
 #include <QToolButton>
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QProcess>
 
 void butts(QWidget *pizdadeda) {
 
     QToolButton *toolButton1 = new QToolButton(pizdadeda);
-    toolButton1->setText("Pizda Deda Flac");
+    toolButton1->setText("Clone Repository");
     toolButton1->setIcon(QIcon(":/NekoSource/bomzi.png"));
     toolButton1->setIconSize(QSize(50, 50));
     toolButton1->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
-    toolButton1->show();
 
-    QToolButton* toolButton2 = new QToolButton(pizdadeda);
-    toolButton2->setText("Ebat Ochko Nigers");
-    toolButton2->setIcon(QIcon(":/NekoSource/gena.png"));
-    toolButton2->setIconSize(QSize(50, 50));
-    toolButton2->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
-    toolButton2->move(90, 0);
-    toolButton2->show();
+    QObject::connect(toolButton1, &QToolButton::clicked, [&]() {
+        QWidget *newWindow = new QWidget();
+        newWindow->setAttribute(Qt::WA_DeleteOnClose);
+
+        QInputDialog inputDialog;
+        bool ok;
+        inputDialog.resize(QSize(300, 200));
+        QString text = inputDialog.getText(newWindow,
+            "Clone Repository",
+            "Enter repository link:",
+            QLineEdit::Normal,
+            "",
+            &ok);
+        if (ok && !text.isEmpty()) {
+            QStringList parts = text.split('/');
+            QString repoName = parts.last().section('.', 0, 0);
+
+            QString command = "git clone " + text + " repos/" + repoName;
+            QProcess* process = new QProcess(newWindow);
+            process->startDetached(command);
+        }
+    });
+
+    toolButton1->show();
 }
