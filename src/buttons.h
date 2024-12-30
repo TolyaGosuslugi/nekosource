4a298aa2d57e351df6232b215a969459700b020c
TolyaGosuslugi <nikita2010belov@gmail.com>
2024-12-30 19:48:22 +0500

    ебана рот

--- src/buttons.h
+++ src/buttons.h
@@ -16,7 +16,13 @@ void butts(QWidget *pizdadeda) {
     toolButton1->setIcon(QIcon(":/NekoSource/bomzi.png"));
     toolButton1->setIconSize(QSize(50, 50));
     toolButton1->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
-    //toolButton->resize(100, 100);
     toolButton1->show();
 
+    QToolButton* toolButton2 = new QToolButton(pizdadeda);
+    toolButton2->setText("Ebat Ochko Nigers");
+    toolButton2->setIcon(QIcon(":/NekoSource/gena.png"));
+    toolButton2->setIconSize(QSize(50, 50));
+    toolButton2->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    toolButton2->move(90, 0);
+    toolButton2->show();
 }
